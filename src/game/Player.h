9a9cef060aceb4c0449c3bc0913ceca1999cca7f
walerik11 <walerik63@yandex.ru>
2020-07-01 16:03:49 +0400

    Revert "max gold"
    This reverts commit 2de6c67f6df32232d160fc710a4d9c9404e3c3e7.

--- src/game/Player.h
+++ src/game/Player.h
@@ -739,7 +739,7 @@ enum PlayerDelayedOperations
 
 // Player summoning auto-decline time (in secs)
 #define MAX_PLAYER_SUMMON_DELAY                   (2*MINUTE)
-#define MAX_MONEY_AMOUNT                       (38D7EA4C67FFF-1)
+#define MAX_MONEY_AMOUNT                       (0x7FFFFFFF-1)
 
 struct InstancePlayerBind
 {
